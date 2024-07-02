23a848d9847553f85e6186714a9807fe6740aac1
Rochak Saini <rochak1rochak@gmail.com>
2024-07-02 22:08:56 +0530

    added db, migrated using knex

--- migrations/20240702163225_initial_setup.js
+++ migrations/20240702163225_initial_setup.js
@@ -1,15 +1,46 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+// migrations/YYYYMMDDHHMMSS_initial_setup.js
 exports.up = function(knex) {
+    return knex.schema
+      .createTable('family_members', table => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.integer('age').notNullable();
+        table.date('date_of_birth').notNullable();
+        table.date('date_of_death');
+        table.string('image');
+        table.text('description');
+      })
+      .createTable('parent_child_relationships', table => {
+        table.increments('id').primary();
+        table.integer('parent_id').references('id').inTable('family_members');
+        table.integer('child_id').references('id').inTable('family_members');
+      })
+      .createTable('spouse_relationships', table => {
+        table.increments('id').primary();
+        table.integer('spouse1_id').references('id').inTable('family_members');
+        table.integer('spouse2_id').references('id').inTable('family_members');
+      })
+      .createTable('traditions_customs', table => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.text('description');
+        table.string('media');
+      })
+      .createTable('events', table => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.text('description');
+        table.date('date').notNullable();
+        table.string('media');
+      });
+  };
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTable('events')
+      .dropTable('traditions_customs')
+      .dropTable('spouse_relationships')
+      .dropTable('parent_child_relationships')
+      .dropTable('family_members');
+  };
   
-};
